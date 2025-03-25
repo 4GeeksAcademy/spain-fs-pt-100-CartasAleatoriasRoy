60b848f34663b40054ae37ead6e02357944e6529
hedrickantonio <rhantoni.cuba@gmail.com>
2025-03-25 15:52:24 +0000

    actualizacion

--- src/app.js
+++ src/app.js
@@ -5,34 +5,9 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  simbolo.innerHTML == '♦'|| simbolo.innerHTML == '♥'? simbolo.style.color ="red": simbolo.style.color ="black"   
-    simbolo2.style.color = simbolo.style.color
-    NumeroCarta.style.color = simbolo.style.color
-
-    NumeroCarta.innerHTML = resultadoCarta(NumeroCartas);
-    simbolo.innerHTML = resultadoCarta(Palos);
-    
-   simbolo2.innerHTML = simbolo.innerHTML
-};
-
-let NumeroCartas = [1,2,3,4,5,6,7,8,9,10,11,12];
+let NumeroCartas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 let Palos = ["♦", "♥", "♠", "♣"];
 let counter = 0;
-
-
-const Randomnize =(arr)=>{
-    return Math.floor(Math.random()* arr.length)
-}
-console.log(Randomnize(NumeroCartas));
-console.log(Randomnize(Palos));
-
-const resultadoCarta = arr => {
-    return arr[Randomnize(arr)]
-    
-      }
-      console.log(resultadoCarta(NumeroCartas))
-      console.log(resultadoCarta(Palos))
 
 let simbolo = document.querySelector("#simbolo");
 let NumeroCarta = document.getElementById("NumeroCarta");
@@ -41,32 +16,48 @@ let simbolo2 = document.getElementById("simbolo2");
 let contador = document.getElementById("texto");
 
 
-reset.addEventListener('click', (e)=>{
-counter ++;
-    
-     simbolo.innerHTML == '♦'|| simbolo.innerHTML == '♥'? simbolo.style.color ="red": simbolo.style.color ="black"   
-    simbolo2.style.color = simbolo.style.color
-    NumeroCarta.style.color = simbolo.style.color
+const randomnize = (arr) => {
+  return Math.floor(Math.random() * arr.length)
+}
+console.log(randomnize(NumeroCartas));
+console.log(randomnize(Palos));
 
-    NumeroCarta.innerHTML = resultadoCarta(NumeroCartas);
-    simbolo.innerHTML = resultadoCarta(Palos);
-    
-   simbolo2.innerHTML = simbolo.innerHTML
-   contador.innerHTML = "Numero de cartas generadas "+counter;
+const resultadoCarta = arr => {
+  return arr[randomnize(arr)]
 
+}
+console.log(resultadoCarta(NumeroCartas))
+console.log(resultadoCarta(Palos))
 
-    
-})
+const createCard = ()=> {
+  NumeroCarta.innerHTML = resultadoCarta(NumeroCartas);
+  simbolo.innerHTML = resultadoCarta(Palos);
 
-setInterval(function(){
- 
-  simbolo.innerHTML == '♦'|| simbolo.innerHTML == '♥'? simbolo.style.color ="red": simbolo.style.color ="black"   
+  simbolo2.innerHTML = simbolo.innerHTML
+  simbolo.innerHTML == '♦' || simbolo.innerHTML == '♥' ? simbolo.style.color = "red" : simbolo.style.color = "black"
   simbolo2.style.color = simbolo.style.color
   NumeroCarta.style.color = simbolo.style.color
 
-  NumeroCarta.innerHTML = resultadoCarta(NumeroCartas);
-  simbolo.innerHTML = resultadoCarta(Palos);
   
- simbolo2.innerHTML = simbolo.innerHTML
-}, 2000);
+}
+
+
+
+window.onload = createCard(); 
+  
+
+
+
+reset.addEventListener('click', (e) => {
+  counter++;
+
+  createCard();
+  contador.innerHTML = "Numero de cartas generadas " + counter;
+
+  clearInterval(Tiempofuera);
+
+
+})
+
+const Tiempofuera = setInterval( createCard, 2000);
 
